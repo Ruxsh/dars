1d5e40bed3065696e7ddb2e77edf5cc637e1e005
Black-monster1 <otabekruhshona@gmail.com>
2024-02-29 20:20:47 +0500

    mansz

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>hola</h1>
+        <h1>hola</h1>
+        <h1>hola</h1>
+        <h1>hola</h1>
+
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
       </header>
     </div>
   );
